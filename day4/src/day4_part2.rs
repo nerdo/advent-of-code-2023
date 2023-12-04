6a72f6b6762f8775b389c1dada458bcefc9ea0d1
Dannel Albert <cuebix@gmail.com>
2023-12-04 17:30:10 -0500

    feat(day4): add part 2 solution

--- day4/src/day4_part2.rs
+++ day4/src/day4_part2.rs
@@ -7,6 +7,6 @@ fn main() {
         .expect("Please supply an input file as the first argument");
     let input = std::fs::read_to_string(&args[1])
         .unwrap_or_else(|err| panic!("Error reading file '{file_path}': {err:?}"));
-    let value = scratch_cards::get_total_scratch_card_points(&input);
-    println!("Sum of scratch card points = {value}");
+    let value = scratch_cards::get_total_number_of_scratch_cards(&input);
+    println!("Total number of cards = {value}");
 }
--- day4/src/scratch_cards.rs
+++ day4/src/scratch_cards.rs
@@ -2,7 +2,7 @@
 #![warn(missing_docs)]
 #![warn(clippy::unwrap_used)]
 
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 #[cfg(test)]
 mod test {
@@ -23,11 +23,33 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
 
         assert_eq!(result, 13);
     }
+
+    #[test]
+    fn day4_get_total_number_of_scratch_cards_returns_the_correct_answer() {
+        let input = r"
+Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+        ";
+
+        let result = get_total_number_of_scratch_cards(input);
+
+        assert_eq!(result, 30);
+    }
+}
+
+/// Gets the total number of scratch cards after processing them with the correct rules.
+pub fn get_total_number_of_scratch_cards(scratch_card_table: &str) -> u64 {
+    let scratch_card_data = ScratchCardData::from(scratch_card_table);
+    scratch_card_data.get_total_number_of_scratch_cards()
 }
 
 /// Gets the total number of points from the scratch card data.
-pub fn get_total_scratch_card_points(scratchcard_table: &str) -> u64 {
-    let scratch_card_data = ScratchCardData::from(scratchcard_table);
+pub fn get_total_scratch_card_points(scratch_card_table: &str) -> u64 {
+    let scratch_card_data = ScratchCardData::from(scratch_card_table);
     scratch_card_data.get_total_points()
 }
 
@@ -38,6 +60,26 @@ struct ScratchCardData {
 impl ScratchCardData {
     fn get_total_points(&self) -> u64 {
         self.card_data.iter().map(|card| card.get_points()).sum()
+    }
+
+    fn get_total_number_of_scratch_cards(&self) -> u64 {
+        let mut counters: HashMap<u64, u64> =
+            self.card_data.iter().map(|card| (card.id, 1_u64)).collect();
+
+        for card in &self.card_data {
+            let num_current_card = *counters
+                .get(&card.id)
+                .unwrap_or_else(|| panic!("Card #{} was not found!", card.id));
+            let num_matches = card.get_num_matches();
+            for won_card_id in card.id + 1..card.id + 1 + num_matches {
+                *counters
+                    .get_mut(&won_card_id)
+                    .unwrap_or_else(|| panic!("Card #{won_card_id} was not found!")) +=
+                    num_current_card;
+            }
+        }
+
+        counters.values().sum()
     }
 }
 
@@ -64,12 +106,15 @@ struct CardData {
 }
 
 impl CardData {
-    fn get_points(&self) -> u64 {
-        let num_winning_numbers = self
-            .actual_numbers
+    fn get_num_matches(&self) -> u64 {
+        self.actual_numbers
             .iter()
             .filter(|n| self.winning_numbers.contains(n))
-            .count() as u32;
+            .count() as u64
+    }
+
+    fn get_points(&self) -> u64 {
+        let num_winning_numbers = self.get_num_matches() as u32;
         match num_winning_numbers {
             0 => 0,
             _ => 2_u64.pow(num_winning_numbers - 1),
